301a2582298925a57864a3495f7d6bcce6271660
Martin Sandström <martin@marteinn.se>
2021-02-13 13:58:41 +0100

    Fix isOffline detection in withOfficeSupport decorator

--- src/decorators.js
+++ src/decorators.js
@@ -2,7 +2,7 @@ const AWS = require("aws-sdk");
 
 const withOfflineSupport = handler => {
     return async (event, ...params) => {
-        if (event.isOffline) {
+        if (process.env.IS_OFFLINE) {
             AWS.config.update({
                 endpoint: "http://localhost:7955",
                 accessKeyId: "AKID",
